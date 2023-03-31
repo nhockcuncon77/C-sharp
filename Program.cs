e3bd4f00ae0a492c9837132a973675b24ea815d4
Viet Huynh <88334618+nhockcuncon77@users.noreply.github.com>
2023-03-30 21:24:13 -0400

    Delegate

--- Program.cs
+++ Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
-using MyNameSpace;
 using static System.Console;
 
-namespace my_example
+namespace Delegate
 {
+    public delegate void ShowLog(string message);
     class Program
     {
+        static void Info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static int Tong(int a, int b) => a + b;
         static void Main(string[] args)
         {
-            Sanpham.Product product = new Sanpham.Product();
+            // ShowLog log = null;
 
-            product.name = "Ipad";
-            product.price = 1000;
-            product.description = "Day la ipad";
+            // log += Info;
+            // log += Info;
+            // log += Info;
+            // log += Warning;
+            // log += Warning;
 
-            product.manufactory = new Sanpham.Product.Manufactory();
-            product.manufactory.name = "zzzz";
-            product.manufactory.addr = "123 li tu trong";
+            // log?.Invoke("Xin chao");
 
-            WriteLine(product.manufactory.getInfoManu());
+            // action
+
+            // Action action;
+            // Action<string> action2;
+
+            // action2 = Warning;
+            // action2 += Info;
+            // action2?.Invoke("Thong bao");
+            // Func<int> f1; //delegate int func()
+
+            Func<int, int, int> tinhtoan;
+            int a = 5;
+            int b = 10;
+
+            tinhtoan = Tong;
+            Console.WriteLine($"KQ:  {tinhtoan(a, b)}");
         }
     }
 }
